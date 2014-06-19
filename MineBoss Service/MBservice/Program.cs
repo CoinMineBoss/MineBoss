a7b12ac25e3a021b0bb728335f2cc98bca26c69c
Shawn L. Djernes <sdjernes@gmail.com>
2014-06-19 17:32:03 -0500

    Setup Stuff

--- MineBoss Service/MBservice/Program.cs
+++ MineBoss Service/MBservice/Program.cs
@@ -19,8 +19,10 @@
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System;
+using System.Configuration;
+using System.ServiceModel;
 
-namespace MBservice
+namespace MineBoss.MBservice
 {
     class MainClass
     {
